1c09136773f5732897c52100844f4421eb448e40
mknowles <mknowles@mit.edu>
2016-11-08 17:49:27 -0500

    Plots work for x,y,z,roll,pitch,yaw

--- scripts/PositionOrientationError.py
+++ scripts/PositionOrientationError.py
@@ -58,10 +58,11 @@ class syncedPose(object):
 		return (self.estTFMsg.transform.translation.x, self.estTFMsg.transform.translation.y, self.estTFMsg.transform.translation.z)
 
 	def getTruthEulerAngles(self):
-		return quat2euler((self.truthTFMsg.transform.rotation.w, self.truthTFMsg.transform.rotation.x, self.truthTFMsg.transform.rotation.y, self.truthTFMsg.transform.rotation.z))
-
+		roll,pitch,yaw = quat2euler((self.truthTFMsg.transform.rotation.w, self.truthTFMsg.transform.rotation.x, self.truthTFMsg.transform.rotation.y, self.truthTFMsg.transform.rotation.z))
+		return (math.degrees(roll),math.degrees(pitch),math.degrees(yaw))
 	def getEstEulerAngles(self):
-		return quat2euler((self.estTFMsg.transform.rotation.w, self.estTFMsg.transform.rotation.x, self.estTFMsg.transform.rotation.y, self.estTFMsg.transform.rotation.z))
+		roll, pitch, yaw = quat2euler((self.estTFMsg.transform.rotation.w, self.estTFMsg.transform.rotation.x, self.estTFMsg.transform.rotation.y, self.estTFMsg.transform.rotation.z))
+		return (math.degrees(roll),math.degrees(pitch),math.degrees(yaw))
 
 	def getSyncedTime(self):
 		return self.syncedTime
@@ -69,6 +70,25 @@ class syncedPose(object):
 	def getEstRotationMatrix(self):
 		return quat2mat((self.estTFMsg.transform.rotation.w, self.estTFMsg.transform.rotation.x, self.estTFMsg.transform.rotation.y, self.estTFMsg.transform.rotation.z))
 
+def removeDiscontinuitiesFromRotation(rotationArray):
+	"""
+	Takes in an array of angles, and fixes discontinuity jumps caused by angles wrapping around 360 degrees.
+	"""
+	offset = 0
+
+	#note: ignore the 1st element in the array because it will consider index -1 (last item in the array)
+	for i in range(1, len(rotationArray)):
+
+		#if the angle suddently jumps by more than 300 degrees, assume that we're wrapping around
+		#shift up or down by 360 degrees as needed
+		if rotationArray[i] - rotationArray[i-1] > 300:
+			offset = -360
+		elif rotationArray[i] - rotationArray[i-1] < -300:
+			offset = 360
+		else:
+			offset = 0
+		rotationArray[i] += offset
+	return rotationArray
 
 def buildSyncedPoseList(bagfile, epsilon, truth_tf, est_tf):
 	"""
@@ -115,7 +135,7 @@ def buildSyncedPoseList(bagfile, epsilon, truth_tf, est_tf):
 
 	return syncedPoses
 
-def getPositionAndRotationArrays(syncedPoses):
+def getPositionAndRotationArrays(syncedPoses, xReverse=False, yReverse=False, zReverse=False, rollReverse=0, pitchReverse=0, yawReverse=0, rmvRotationDisc=True):
 	#times stores the timestamps in order for the synced poses
 	times = []
 
@@ -140,14 +160,16 @@ def getPositionAndRotationArrays(syncedPoses):
 	z_offset = 0
 	#now go through every synced pose and extract the right information
 	counter = 0
+	good_ye = []
 	for i in syncedPoses:
 		times.append(i.getSyncedTime())
 
 		xt, yt, zt = i.getTruthXYZ()
 		xe, ye, ze = i.getEstXYZ()
+		good_ye.append(ye)
 
 		roll_t, pitch_t, yaw_t = i.getTruthEulerAngles()
-		roll_est, pitch_est, yaw_est = i.getEstEulerAngles()
+		roll_e, pitch_e, yaw_e = i.getEstEulerAngles()
 
 		#add the truth coordinates to their respective arrays
 		truth_roll.append(roll_t)
@@ -163,31 +185,60 @@ def getPositionAndRotationArrays(syncedPoses):
 			x_offset = xt-xe
 			y_offset = yt-ye
 			z_offset = zt-ze
+			roll_offset = roll_t - roll_e
+			pitch_offset = pitch_t - pitch_e
+			print("Pitch offset:", pitch_offset)
+			yaw_offset = yaw_t - yaw_e
 			counter += 1
-			print("Offsets:", x_offset, y_offset, z_offset)
+			print("Offsets:", x_offset, y_offset, z_offset, roll_offset, pitch_offset, yaw_offset)
 			print("Initial Truth Eulers:", roll_t, pitch_t, yaw_t)
-			print("Initial Est. Eulers:", roll_est, pitch_est, yaw_est)
+			print("Initial Est. Eulers:", roll_e, pitch_e, yaw_e)
 
 		#add the estimated coordinates to their respective arrays
-		est_x.append(-1*xe+x_offset)
-		est_y.append(ye) #need to multiply x and y by -1 because TF conventions are different for IMU and Vicon
+		if xReverse:
+			xe*=-1
+		if yReverse:
+			ye*=-1
+		if zReverse:
+			ze*=-1
+		if rollReverse:
+			roll_e*=-1
+		if pitchReverse:
+			pitch_e*=-1
+		if yawReverse:
+			yaw_e*=-1
+
+		est_x.append(xe+x_offset)
+		est_y.append(ye+y_offset) #need to multiply x and y by -1 because TF conventions are different for IMU and Vicon
 		est_z.append(ze+z_offset)
-		est_roll.append(roll_est)
-		est_pitch.append(pitch_est)
-		est_yaw.append(yaw_est)
+		est_roll.append(roll_e+roll_offset)
+		est_pitch.append(pitch_e+pitch_offset)
+		est_yaw.append(yaw_e+yaw_offset)
+
+	#est_y data is good leaving this function!!!!
+
+	if rmvRotationDisc:
+		est_roll = removeDiscontinuitiesFromRotation(est_roll)
+		est_pitch = removeDiscontinuitiesFromRotation(est_pitch)
+		est_yaw = removeDiscontinuitiesFromRotation(est_yaw)
 
 	return (est_x, est_y, est_z, est_roll, est_pitch, est_yaw, truth_x, truth_y, truth_z, truth_roll, truth_pitch, truth_yaw, times)
 
 
-def plotEstimationVSTruth(syncedPoses, x=True,y=True,z=True,roll=True,pitch=True,yaw=True):
+def plotEstimationVSTruth(syncedPoses, x=0,y=0,z=0,roll=True,pitch=True,yaw=True):
 	"""
 	Plots each coordinate vs. truth over time.
 	Changing the parameter flags will determine which plots are shown.
 	syncedPoses: an array of synced pose objects
 	"""
-	xe, ye, ze, re, pe, ye, xt, yt, zt, rt, pt, yt, times = getPositionAndRotationArrays(syncedPoses)
-	print len(xe)
-	assert(len(ye)==len(times))
+	xe, ye, ze, roll_e, pitch_e, yaw_e, xt, yt, zt, roll_t, pitch_t, yaw_t, times = getPositionAndRotationArrays(syncedPoses,xReverse=True,yReverse=True)
+	
+	#make sure the data is 1-to-1 by running a few tests
+	# assert(len(ye)==len(times),"Mismatch in number of time values and position estimate values")
+	# assert(len(yt)==len(times),"Mismatch in number of time values and position truth values")
+	# assert(len(yaw_e)==len(times), "Mismatch in number of time values and rotation estimate values")
+	# assert(len(pitch_t)==len(times), "Mismatch in number of time values and rotation truth values")
+
 
 	if x:
 		#Plot 1: X vs. truth
@@ -196,13 +247,21 @@ def plotEstimationVSTruth(syncedPoses, x=True,y=True,z=True,roll=True,pitch=True
 		plt.subplot(211)
 		plt.plot(times, xe,'r', times, xt, 'y')
 
+		plt.subplot(212)
+		x_err = [xe[i]-xt[i] for i in range(len(xe))]
+		plt.plot(times,x_err,'m')
+
 	if y:
 		#Plot 2: Y vs. truth
-		print(ye)
+		#print(ye)
 		plt.figure(2)
 		plt.title("Y vs. Truth")
 		plt.subplot(211)
 		plt.plot(times, ye,'g', times, yt,'y')
+
+		plt.subplot(212)
+		y_err = [ye[i]-yt[i] for i in range(len(ye))]
+		plt.plot(times,y_err,'m')
 
 	if z:
 		#Plot 2: Y vs. truth
@@ -211,24 +270,50 @@ def plotEstimationVSTruth(syncedPoses, x=True,y=True,z=True,roll=True,pitch=True
 		plt.subplot(211)
 		plt.plot(times, ze,'b', times, zt,'y')
 
+		plt.subplot(212)
+		z_err = [ze[i]-zt[i] for i in range(len(ze))]
+		plt.plot(times,z_err,'m')
+
 	if roll:
-		pass
+		plt.figure(4)
+		plt.title("Roll vs. Truth")
+		plt.subplot(211)
+		plt.plot(times, roll_e, 'r', times, roll_t, 'y')
+
+		plt.subplot(212)
+		roll_err = [roll_e[i]-roll_t[i] for i in range(len(roll_e))]
+		plt.plot(times,roll_err,'m')
 
 	if pitch:
-		pass
+		plt.figure(5)
+		plt.title("Pitch vs. Truth")
+		plt.subplot(211)
+		plt.plot(times, pitch_e, 'g', times, pitch_t, 'y')
+
+		plt.subplot(212)
+		pitch_err = [pitch_e[i]-pitch_t[i] for i in range(len(pitch_e))]
+		plt.plot(times,pitch_err,'m')
 
 	if yaw:
-		pass
+		plt.figure(6)
+		plt.title("Yaw vs. Truth")
+		plt.subplot(211)
+		plt.plot(times, yaw_e, 'b', times, yaw_t, 'y')
 
-		plt.show()
+		plt.subplot(212)
+		yaw_err = [yaw_e[i]-yaw_t[i] for i in range(len(yaw_e))]
+		plt.plot(times,yaw_err,'m')
+
+	plt.show()
 
 
-def plotEstimationError(x_error=True,y_error=True,z_error=True,roll_error=True,pitch_error=True,yaw_error=True):
-	pass
+
+
+	
 
 def main():
 	#get a chronological list of synced poses from the bag
-	syncedPoses = buildSyncedPoseList(EASY_RECORD_FILE, 100000000, truth_tf, est_tf)
+	syncedPoses = buildSyncedPoseList(EASY_RECORD_FILE, 10000000, truth_tf, est_tf)
 	print("Created", len(syncedPoses), "synced poses")
 
 	#display the right plots
