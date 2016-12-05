011ed8c26a2fb597e5c40aa9e3b657ca45342fba
mknowles <mknowles@mit.edu>
2016-12-05 13:19:15 -0500

    Points seem to be aligned in xy plane, but a little off...

--- scripts/3DPoseViz.py
+++ scripts/3DPoseViz.py
@@ -1,19 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+#ros modules
 import rosbag
 import roslib
 import rospy
+import tf
+
+#plotting modules
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import pylab as p
 from mpl_toolkits.mplot3d import Axes3D
+
+#math modules
 import math
-from transforms3d.quaternions import rotate_vector, quat2mat
+from transforms3d.quaternions import rotate_vector, quat2mat, qinverse, qmult
 from transforms3d.euler import quat2euler
 import numpy as np
 
 #message type for Vicon truth pose data, and the rovio estimated pose
-from geometry_msgs.msg import TransformStamped
+from geometry_msgs.msg import TransformStamped, PointStamped, QuaternionStamped, PoseStamped
 
 
 def areSynchronized(msg1, msg2, epsilon):
@@ -32,30 +39,66 @@ def areSynchronized(msg1, msg2, epsilon):
 
 
 class syncedPose(object):
-	def __init__(self, truthTFMsg, estTFMsg):
+	def __init__(self, truthTFMsg, estPoseMsg):
 		self.truthTFMsg = truthTFMsg
-		self.estTFMsg = estTFMsg
+		self.estTFMsg = estPoseMsg #note: estTFMsg is of type nav_msgs/Odometry
 		self.truthTime = float(self.truthTFMsg.header.stamp.secs) + float(self.truthTFMsg.header.stamp.nsecs) / 1e9
 		self.estTime = float(self.estTFMsg.header.stamp.secs) + float(self.estTFMsg.header.stamp.nsecs) / 1e9
 		self.syncedTime = (self.truthTime + self.estTime) / 2
 
+	def pp(self):
+		print("TruthXYZ:", self.getTruthXYZ())
+		print("EstXYZ:", self.getEstXYZ())
+		print("TruthQuat:", self.getTruthQuat())
+		print("EstQuat:", self.getEstQuat())
+		print("Truth Euler:", self.getTruthEulerAngles())
+		print("Est Euler:", self.getEstEulerAngles())
+
+	def getEstTransform(self):
+		tfm = TransformStamped()
+		tfm.header = self.estTFMsg.header
+		tfm.child_frame_id = self.estTFMsg.child_frame_id
+		tfm.transform.translation.x, tfm.transform.translation.y, tfm.transform.translation.z = self.getEstXYZ()
+		tfm.transform.rotation.w, tfm.transform.rotation.x, tfm.transform.rotation.y, tfm.transform.rotation.z = self.getEstQuat()
+		return tfm
+
+	def getEstTF(self):
+		return self.estTFMsg
+
+	def getTruthTF(self):
+		return self.truthTFMsg
+
+	def transformPose(self, tf_ros, truth_frame_name, world_frame_name, tf_time):
+		"""
+		tf_ros: a TransformerROS object with the the necessary frames set up 
+		This function transforms the quad's pose to the world frame, and then to the vicon frame
+		"""
+		pose = PoseStamped()
+		pose.header = self.estTFMsg.header
+		pose.header.stamp = tf_time
+		pose.pose = self.estTFMsg.pose.pose
+		pose = tf_ros.transformPose(world_frame_name, pose)
+		pose = tf_ros.transformPose(truth_frame_name, pose)
+		return pose # returns the pose part of of the msg
+
+	def getTruthQuat(self):
+		return (self.truthTFMsg.transform.rotation.w, self.truthTFMsg.transform.rotation.x, self.truthTFMsg.transform.rotation.y, self.truthTFMsg.transform.rotation.z)
+	def getEstQuat(self):
+		return (self.estTFMsg.pose.pose.orientation.w, self.estTFMsg.pose.pose.orientation.x, self.estTFMsg.pose.pose.orientation.y, self.estTFMsg.pose.pose.orientation.z)
 	def getTruthXYZ(self):
 		return (self.truthTFMsg.transform.translation.x, self.truthTFMsg.transform.translation.y, self.truthTFMsg.transform.translation.z)
-
 	def getEstXYZ(self):
-		return (self.estTFMsg.transform.translation.x, self.estTFMsg.transform.translation.y, self.estTFMsg.transform.translation.z)
-
+		return (self.estTFMsg.pose.pose.position.x, self.estTFMsg.pose.pose.position.y, self.estTFMsg.pose.pose.position.z)
 	def getTruthEulerAngles(self):
-		return quat2euler((self.truthTFMsg.transform.rotation.w, self.truthTFMsg.transform.rotation.x, self.truthTFMsg.transform.rotation.y, self.truthTFMsg.transform.rotation.z))
-
+		return [math.degrees(i) for i in quat2euler((self.truthTFMsg.transform.rotation.w, self.truthTFMsg.transform.rotation.x, self.truthTFMsg.transform.rotation.y, self.truthTFMsg.transform.rotation.z))]
 	def getEstEulerAngles(self):
-		return quat2euler((self.estTFMsg.transform.rotation.w, self.estTFMsg.transform.rotation.x, self.estTFMsg.transform.rotation.y, self.estTFMsg.transform.rotation.z))
+		return [math.degrees(i) for i in quat2euler((self.estTFMsg.pose.pose.orientation.w, self.estTFMsg.pose.pose.orientation.x, self.estTFMsg.pose.pose.orientation.y, self.estTFMsg.pose.pose.orientation.z))]
 
-	def getEstRotationMatrix(self):
-		return quat2mat((self.estTFMsg.transform.rotation.w, self.estTFMsg.transform.rotation.x, self.estTFMsg.transform.rotation.y, self.estTFMsg.transform.rotation.z))
+	# def getEstRotationMatrix(self):
+	# 	return quat2mat((self.estTFMsg.transform.rotation.w, self.estTFMsg.transform.rotation.x, self.estTFMsg.transform.rotation.y, self.estTFMsg.transform.rotation.z))
 
 
-def buildSyncedPoseList(bagfile, epsilon, truth_topic, est_topic):
+def buildSyncedPoseList(bagfile, epsilon, truth_topic, est_topic, pose_topic):
 	"""
 	Bagfile: the full path to the .bag file that contains truth/est transforms
 	epsilon: the value (nanosecs) that two msg stamps must be within to be considered simultaneous
@@ -69,7 +112,8 @@ def buildSyncedPoseList(bagfile, epsilon, truth_topic, est_topic):
 	syncedPoses = []
 
 	#topic, message data object, time
-	for topic, msg, t in bag.read_messages(topics=[truth_topic, est_topic]):
+	counter = 0
+	for topic, msg, t in bag.read_messages(topics=[truth_topic, pose_topic]):
 
 		#see if we're already holding a msg to find a match
 		if holdMsg == None:
@@ -141,15 +185,23 @@ STAR1 = '/home/mknowles/bagfiles/star/star1_rovio.bag'
 STAR2 = '/home/mknowles/bagfiles/star/star2_rovio.bag'
 # END BAGFILE DEFS #
 
+WORLD_FRAME_NAME = 'world'
+VICON_FRAME_NAME = 'vicon/firefly_sbx/firefly_sbx'
+QUAD_FRAME_NAME = 'imu'
+
 def main():
+
+	#make a TransformerROS object (handles transforms operations)
+	t = tf.TransformerROS(True, rospy.Duration(10))
 
 	#SETUP
 	BAGFILE = STAR1 #the full path to the bagfile
 	TRUTH_TF = '/vicon/tf' #the name of the truth transform topic
 	EST_TF = '/rovio/transform' # the name of the estimated transform (odometry for rovio) topic
+	POSE_TOPIC = '/rovio/odometry'
 
 	#get a chronological list of synced poses from the bag
-	syncedPoses = buildSyncedPoseList(BAGFILE, 1e8, TRUTH_TF, EST_TF)
+	syncedPoses = buildSyncedPoseList(BAGFILE, 1e7, TRUTH_TF, EST_TF, POSE_TOPIC)
 	print("Plotting", len(syncedPoses), "synchronized poses.")
 
 	#create an MPL figure
@@ -168,41 +220,51 @@ def main():
 	x_offset = 0
 	y_offset = 0
 	z_offset = 0
+	roll_offset = 0
+	pitch_offset = 0
+	yaw_offset = 0
 
 	counter = 0
 	for i in syncedPoses:
 		xt, yt, zt = i.getTruthXYZ()
 		xe, ye, ze = i.getEstXYZ()
 
-		roll_t, pitch_t, yaw_t = i.getTruthEulerAngles()
-		roll_est, pitch_est, yaw_est = i.getEstEulerAngles()
+		# roll_t, pitch_t, yaw_t = i.getTruthEulerAngles()
+		# roll_e, pitch_e, yaw_e = i.getEstEulerAngles()
 
-		truth_x.append(xt)
-		truth_y.append(yt)
-		truth_z.append(zt)
-		#signs between IMU and truth x,y are flipped!
-		# xe*=-1
-		# ye*=-1
-
-		if counter==0:
-			x_offset = xt-xe
-			y_offset = yt-ye
-			z_offset = zt-ze
-			est_eulers = i.getEstEulerAngles()
-			truth_eulers = i.getTruthEulerAngles()
+		if counter<1:
+			#calculate relative quaternion
+			quat_rel = qmult(qinverse(np.array(i.getEstQuat())), np.array(i.getTruthQuat()))
+			rel_rot_mat = quat2mat(quat_rel)
+			x_est_offset = 0-xe
+			y_est_offset = 0-ye
+			z_est_offset = 0-ze
+			x_truth_offset = 0-xt
+			y_truth_offset = 0-yt
+			z_truth_offset = 0-zt
+			# roll_offset = roll_t - roll_e
+			# pitch_offset = pitch_t - roll_t
+			# yaw_offset = yaw_t - roll_t
 			counter += 1
-			callibrationRotMat = i.getEstRotationMatrix()
-			print(callibrationRotMat)
-			print("Truth Eulers:", truth_eulers)
-			print("Est. Eulers:", est_eulers)
 
-		translated_xyz = (xe+x_offset, ye+y_offset, ze+z_offset)
-		# rotated_xyz = offsetRotation(callibrationRotMat, translated_xyz)
-		# print "Rotated", rotated_xyz
+			i.pp()
 
-		est_x.append(translated_xyz[0])
-		est_y.append(translated_xyz[1])
-		est_z.append(translated_xyz[2])
+			#determine rotation matrix to apply
+			est_rot_mat = quat2mat(i.getEstQuat())
+			truth_rot_mat = quat2mat(i.getTruthQuat())
+
+		#first translate by some offset, then rotate into the truth coordinate frame
+		translated_xyz_est = np.array([xe+x_est_offset, ye+y_est_offset, ze+z_est_offset])
+		translated_xyz_truth = np.array([xt+x_truth_offset, yt+y_truth_offset, zt+z_truth_offset])
+		rotated_xyz_truth = np.dot(truth_rot_mat, translated_xyz_truth)
+		rel_rotated_xyz_est = rotate_vector(translated_xyz_est, quat_rel)
+
+		est_x.append(-rel_rotated_xyz_est[0])
+		est_y.append(rel_rotated_xyz_est[1])
+		est_z.append(-rel_rotated_xyz_est[2])
+		truth_x.append(translated_xyz_truth[0])
+		truth_y.append(translated_xyz_truth[1])
+		truth_z.append(translated_xyz_truth[2])
 
 	#format the plot: truth is RED, estimated is GREEN
 	ax.set_xlabel('X')
